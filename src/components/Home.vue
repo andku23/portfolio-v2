b2ef39d3145424a55bf2fc8ecc6397fe8d81bcf3
Andrew Kurano <andrew.kurano@triggerglobal.com>
2021-10-17 16:13:38 -0700

    Put in basic routing

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class = 'home-container'>
+
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "Home.vue",
+		data: function () {
+			return {}
+		}
+	}
+</script>
+
+<style scoped lang = "scss">
+    @import "../scss/global.scss";
+    .home-container{
+
+    }
+</style>
